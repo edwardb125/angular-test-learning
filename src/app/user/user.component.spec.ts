a558a3e9b1d52d3e017e8f649fd85c652d787944
erfan hemati <erfanhematig@gmail.com>
2021-09-03 13:58:22 +0430

    in this version added service test

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,25 +1,26 @@
+import { UserService } from './user.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
-  let component: UserComponent;
-  let fixture: ComponentFixture<UserComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UserComponent ]
-    })
-    .compileComponents();
-  });
-
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    TestBed.configureTestingModule({
+      declarations: [UserComponent]
+    })
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should creat the app', () =>{
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  })
+
+  it('should use the user name from the service', () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let userService = fixture.debugElement.injector.get(UserService);
+    fixture.detectChanges();
+    expect(userService.user.name).toEqual(app.user.name);
+  })
 });
